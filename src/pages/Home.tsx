d1af597c48fbdc39816380507730a62fe90084dd
Fred-Reis <fredgost@hotmail.com>
2022-04-09 01:00:09 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box, Text} from 'native-base';
+
+export const Home = () => {
+  return (
+    <Box flex={1} bg="primary.100" alignItems="center" justifyContent="center">
+      <Text color="#715" fontSize={25}>
+        Hello world new app w TS
+      </Text>
+    </Box>
+  );
+};
